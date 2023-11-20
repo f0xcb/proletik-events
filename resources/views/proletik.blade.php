a2c05f4774f17c2686d2aa5b025a11d52d1b72dc
f0xcb <tim.kempe@mailbox.org>
2023-11-20 18:11:34 +0100

    [+] Tailwind, Landingpage

---
+++ resources/views/proletik.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>PROLETIK SONNENBERG</title>
+
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-white dark:bg-black flex items-center justify-center h-screen">
+
+<div class="text-center">
+    <h1 class="text-4xl font-bold bg-gradient-to-r from-blue-800 to-yellow-200 bg-clip-text text-transparent">
+        PROLETIK SONNENBERG</h1>
+</div>
+
+</body>
+</html>
